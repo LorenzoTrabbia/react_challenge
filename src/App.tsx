f138b346147030e761eb28032b9923f449a23a24
Lorenzo Trabbia <lorenzo.trabbia@edu.itspiemonte.it>
2022-11-24 14:52:18 +0100

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+type RowType = {
+    operator: "+" | "-";
+    value: number;
+    disabled: boolean;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [rows, setRow] = useState<RowType[]>([]);
+    const [result, setResult] = useState<number>(0);
+
+    useEffect(() => {
+        setResult(
+            rows.reduce((acc, row) => {
+                if (!row.disabled) {
+                    return row.operator === "+"
+                        ? acc + +row.value
+                        : acc - +row.value;
+                }
+                return acc;
+            }, 0)
+        );
+    }, [rows]);
+
+    const handleAddRow = () => {
+        setRow([...rows, { operator: "+", value: 0, disabled: false }]);
+    };
+
+    const handleRemoveRow = (index: number) => {
+        const newRows = [...rows];
+        newRows.splice(index, 1);
+        setRow(newRows);
+    };
+
+    const handleOperatorChange = (index: number, operator: "+" | "-") => {
+        const newRows = [...rows];
+        newRows[index].operator = operator;
+        setRow(newRows);
+    };
+
+    const handleValueChange = (index: number, value: number) => {
+        const newRows = [...rows];
+        newRows[index].value = value;
+        setRow(newRows);
+    };
+
+    const handleDisabledChange = (index: number) => {
+        const newRows = [...rows];
+        newRows[index].disabled = newRows[index].disabled ? false : true;
+        setRow(newRows);
+    };
+
+    return (
+        <>
+            <div>
+                <button onClick={handleAddRow}>Add row</button>
+            </div>
+            <ul>
+                {rows.map((row, index) => (
+                    <li key={index}>
+                        <select
+                            defaultValue={row.operator}
+                            onChange={(e: any) =>
+                                handleOperatorChange(index, e.target.value)
+                            }
+                        >
+                            <option>+</option>
+                            <option>-</option>
+                        </select>
+                        <input
+                            type="number"
+                            defaultValue={row.value}
+                            onChange={(e: any) =>
+                                handleValueChange(index, e.target.value)
+                            }
+                        />
+                        <button onClick={() => handleRemoveRow(index)}>
+                            Delete
+                        </button>
+                        <button onClick={() => handleDisabledChange(index)}>
+                            {row.disabled ? "Enable" : "Disable"}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <div>Result: {result}</div>
+        </>
+    );
 }
 
 export default App;
