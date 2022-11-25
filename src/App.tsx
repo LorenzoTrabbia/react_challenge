3289214d72f6aa1ccebf7bafdb62acfa5a2b8ed3
Lorenzo Trabbia <lorenzo.trabbia@edu.itspiemonte.it>
2022-11-25 11:53:11 +0100

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+// Type for the row object
 type RowType = {
     operator: "+" | "-";
     value: number;
@@ -11,6 +12,7 @@ function App() {
     const [rows, setRow] = useState<RowType[]>([]);
     const [result, setResult] = useState<number>(0);
 
+    // Function to update the result
     useEffect(() => {
         setResult(
             rows.reduce((acc, row) => {
@@ -24,28 +26,33 @@ function App() {
         );
     }, [rows]);
 
+    // Function to add a new row
     const handleAddRow = () => {
         setRow([...rows, { operator: "+", value: 0, disabled: false }]);
     };
 
+    // Function to remove a row
     const handleRemoveRow = (index: number) => {
         const newRows = [...rows];
         newRows.splice(index, 1);
         setRow(newRows);
     };
 
+    // Function to update the operator of a specific row
     const handleOperatorChange = (index: number, operator: "+" | "-") => {
         const newRows = [...rows];
         newRows[index].operator = operator;
         setRow(newRows);
     };
 
+    // Function to update the value of a specific row
     const handleValueChange = (index: number, value: number) => {
         const newRows = [...rows];
         newRows[index].value = value;
         setRow(newRows);
     };
 
+    // Function to update the disabled state of a specific row
     const handleDisabledChange = (index: number) => {
         const newRows = [...rows];
         newRows[index].disabled = newRows[index].disabled ? false : true;
@@ -54,6 +61,7 @@ function App() {
 
     return (
         <>
+            <h1 className="h1">React challenge - Lorenzo Trabbia</h1>
             <div>
                 <button onClick={handleAddRow} className="button-add">
                     Add row
@@ -63,6 +71,7 @@ function App() {
                 {rows.map((row, index) => (
                     <li key={index}>
                         <select
+                            className="select"
                             defaultValue={row.operator}
                             onChange={(e: any) =>
                                 handleOperatorChange(index, e.target.value)
@@ -73,7 +82,8 @@ function App() {
                         </select>
                         <input
                             type="number"
-                            defaultValue={row.value === 0 ? "" : row.value}
+                            className="input"
+                            value={row.value === 0 ? "" : row.value}
                             onChange={(e: any) =>
                                 handleValueChange(index, e.target.value)
                             }
